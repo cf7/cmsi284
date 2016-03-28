fe7616a749e6e84f9439e0048f11e4e8c7e418ba
cf7 <cfranco7@lion.lmu.edu>
2016-03-28 13:35:07 -0700

    using sprintf to generate madlibs

--- c-intro/madlib-test.c
+++ c-intro/madlib-test.c
@@ -2,19 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <assert.h>
 
-// #include <madlib.c>
+#include "madlib.c"
 
 // screencast 3/17 ?
 
 int main() {
     printf("%s\n", "Testing madlibs from Problem 3");
 
-    assert(madlib("%s %s %s", "brilliant", "git", "swim") != "brilliant git swim");
+    madlib("The %s %s likes to %s in the moonlight.", "brilliant", "git", "swim");
+    madlib("How much %s could a %s %s?", "wood", "woodchuck", "chuck");
+    
+    // assert(madlib("%s %s %s", "brilliant", "git", "swim") != "brilliant git swim");
 
-    assert(madlib("%s %s %s ", "brilliant", "git", "swim") == "brilliant git swim");
+    // assert(madlib("%s %s %s ", "brilliant", "git", "swim") == "brilliant git swim");
 
-    assert(madlib("The %s %s likes to %s in the moonlight.", "brilliant", "git", "swim") == "The brilliant git likes to swim in the moonlight");
+    // assert(madlib("The %s %s likes to %s in the moonlight.", "brilliant", "git", "swim") == "The brilliant git likes to swim in the moonlight");
 
-    assert(madlib("The %s %s likes to %s in the moonlight.", "brilliant", "git", "swim"));
+    // assert(madlib("The %s %s likes to %s in the moonlight.", "brilliant", "git", "swim"));
 }
--- c-intro/madlib.c
+++ c-intro/madlib.c
@@ -1,5 +1,19 @@
 // screencast 3/10 at 32:30
 
+#include <stdio.h>  // %s - strings, %p - pointer
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+
 char* madlib(char* template, char* adjective, char* noun, char* verb) {
-    
+    printf("%s\n", "inside");
+    char *newString = malloc(100);
+    printf("%s\n", "after newString");
+    printf("%s\n", template);
+    sprintf(newString, template, adjective, noun, verb);
+    printf("%s\n", "after sprintf");
+    printf("%s\n", newString);
+    free(newString);
+    return newString;
 }
