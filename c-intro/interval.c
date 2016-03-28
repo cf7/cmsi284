decc13aaa53501b83cf01b40ccdc55bbafebffd5
cf7 <cfranco7@lion.lmu.edu>
2016-03-28 05:36:14 -0700

    computing intervals and accounting for wrap-arounds

--- c-intro/interval.c
+++ c-intro/interval.c
@@ -13,6 +13,7 @@ long notesLength = sizeof(notes) / sizeof(notes[0]);
 char *intervalNames[12] = { "minor second", "major second", "minor third", "major third", 
                         "perfect fourth", "tritone", "perfect fifth", "minor sixth", "major sixth",
                         "minor seventh", "major seventh", "perfect octave" };
+long intervalNamesLength = sizeof(intervalNames) / sizeof(intervalNames[0]);
 
 bool checkArgs(char *arg1, char *arg2) {
     bool validNote1 = false;
@@ -20,10 +21,10 @@ bool checkArgs(char *arg1, char *arg2) {
     printf("%s\n", arg1);
     printf("%s\n", arg2);
     for (int index = 0; index < notesLength; index++) {
-        if (strcmp(notes[index], arg1) == 0) {
+        if (!strcmp(notes[index], arg1)) {
             validNote1 = true;
         }
-        if (strcmp(notes[index], arg2) == 0) {
+        if (!strcmp(notes[index], arg2)) {
             validNote2 = true;
         }
     }
@@ -33,7 +34,7 @@ bool checkArgs(char *arg1, char *arg2) {
 int noteIndex(char *arg) {
     int thisNote;
     for (int index = 0; index < notesLength; index++) {
-        if (strcmp(notes[index], arg) == 0) {
+        if (!strcmp(notes[index], arg)) {
             thisNote = index;
         }
     }
@@ -55,6 +56,18 @@ char* nextNote(int currentIndex, int distance) {
     return note;
 }
 
+char* calcInterval(int note1Index, int note2Index) {
+    if (note2Index < note1Index) {
+        note2Index = note2Index + 12;
+    }
+    int diff = note2Index - note1Index;
+    if (!diff) {
+        return intervalNames[intervalNamesLength - 1];
+    } else {
+        return intervalNames[diff - 1];
+    }
+}
+
 int main(int argc, char** argv) {
     if (argc > 2) {
         char *firstInput = argv[1];
@@ -70,7 +83,7 @@ int main(int argc, char** argv) {
             printf("note1: %d\n", note1);
             printf("note2: %d\n", note2);
             
-            
+            printf("%s to %s is a %s\n", notes[note1], notes[note2], calcInterval(note1, note2));
         }
     } else {
         perror("Please input two arguments separated by spaces");
