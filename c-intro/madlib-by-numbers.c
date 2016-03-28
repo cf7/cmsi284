3e408c38146cdb2d0f0f4819d8e138d5a6dbc60b
cf7 <cfranco7@lion.lmu.edu>
2016-03-28 15:58:36 -0700

    Experimenting with string manipulation

--- c-intro/madlib-by-numbers.c
+++ c-intro/madlib-by-numbers.c
@@ -4,9 +4,20 @@
 #include <stdbool.h>
 #include <assert.h>
 
+char *numbers[10] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+long numbersLength = sizeof(numbers) / sizeof(numbers[0]);
+int ascii[10] = { 48, 49, 50, 51, 52, 53, 54, 55, 56, 57 };
+
+
 
 char* madlib_by_numbers(char* template, int word_count, char* words[]) {
     char* newString;
 
+    printf("%s\n", template);
+    
+    // returns a pointer to the matching byte
+    char *p = memchr(template, '1', strlen(template));
+    printf("%s\n", p);
+    // returns a segfault
     return newString;
 }
