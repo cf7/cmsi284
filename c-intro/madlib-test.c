e641babe1a9b527e036b19ec127d410ca07507ba
cf7 <cfranco7@lion.lmu.edu>
2016-03-28 13:39:37 -0700

    fixed test cases

--- c-intro/madlib-test.c
+++ c-intro/madlib-test.c
@@ -13,12 +13,8 @@ int main() {
 
     madlib("The %s %s likes to %s in the moonlight.", "brilliant", "git", "swim");
     madlib("How much %s could a %s %s?", "wood", "woodchuck", "chuck");
-    
-    // assert(madlib("%s %s %s", "brilliant", "git", "swim") != "brilliant git swim");
 
-    // assert(madlib("%s %s %s ", "brilliant", "git", "swim") == "brilliant git swim");
+    assert(!strcmp(madlib("%s %s %s", "brilliant", "git", "swim"), "brilliant git swim"));
 
-    // assert(madlib("The %s %s likes to %s in the moonlight.", "brilliant", "git", "swim") == "The brilliant git likes to swim in the moonlight");
-
-    // assert(madlib("The %s %s likes to %s in the moonlight.", "brilliant", "git", "swim"));
+    assert(!strcmp(madlib("The %s %s likes to %s in the moonlight.", "brilliant", "git", "swim"), "The brilliant git likes to swim in the moonlight."));
 }
--- c-intro/madlib.c
+++ c-intro/madlib.c
@@ -7,12 +7,8 @@
 #include <assert.h>
 
 char* madlib(char* template, char* adjective, char* noun, char* verb) {
-    printf("%s\n", "inside");
     char *newString = malloc(100);
-    printf("%s\n", "after newString");
-    printf("%s\n", template);
     sprintf(newString, template, adjective, noun, verb);
-    printf("%s\n", "after sprintf");
     printf("%s\n", newString);
     free(newString);
     return newString;
