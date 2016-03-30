74753813e45f4e9f4b8accd20f4fc6cd96620e4e
cf7 <cfranco7@lion.lmu.edu>
2016-03-29 19:23:06 -0700

    setting up files for problem 5

---
+++ c-intro/reverse-words-test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+
+int main() {
+    printf("%s\n", "Testing reverse_words from Problem 5");
+
+    printf("%s\n", "End of tests");
+}
